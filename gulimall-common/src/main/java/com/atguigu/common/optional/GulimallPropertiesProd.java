a8820f5c757b8f426cfe5c994580d4af4c75e2c0
iuwjh <jerrywu555@outlook.com>
2020-09-06 12:23:58 +0800

    添加了H2依赖和配置

---
+++ gulimall-common/src/main/java/com/atguigu/common/optional/GulimallPropertiesProd.java
@@ -0,0 +1,9 @@
+package com.atguigu.common.optional;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:prod.properties")
+public class GulimallPropertiesProd {
+}
