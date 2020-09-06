8501651d0f4e283f5f061c187f8b08e8fead19ed
iuwjh <jerrywu555@outlook.com>
2020-09-06 12:23:58 +0800

    添加了H2依赖和配置

--- gulimall-common/src/main/java/com/atguigu/common/GulimallCommonModule.java
+++ gulimall-common/src/main/java/com/atguigu/common/GulimallCommonModule.java
@@ -1,12 +1,12 @@
 package com.atguigu.common;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 
-@ComponentScan
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*\\.optional\\..*"))
 @Configuration
-@PropertySource("classpath:gulimall-common-shared.properties")
+@PropertySource("classpath:base.properties")
 public class GulimallCommonModule {
 }
---
+++ gulimall-common/src/main/java/com/atguigu/common/optional/GulimallPropertiesDev.java
@@ -0,0 +1,10 @@
+package com.atguigu.common.optional;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@PropertySource("classpath:dev.properties")
+public class GulimallPropertiesDev {
+}
