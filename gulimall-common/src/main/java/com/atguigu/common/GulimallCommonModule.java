a47cb89b525f7d5cc07b46e16ac98e7db4c223b2
iuwjh <jerrywu555@outlook.com>
2020-09-07 20:14:38 +0800

    引入liquibase数据库版本控制 添加数据库初始schema 配置使用lombok 为Spring添加dev,prod profile

--- gulimall-common/src/main/java/com/atguigu/common/GulimallCommonModule.java
+++ gulimall-common/src/main/java/com/atguigu/common/GulimallCommonModule.java
@@ -1,12 +1,31 @@
 package com.atguigu.common;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
 
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*\\.optional\\..*"))
 @Configuration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*\\.optional\\..*"))
 @PropertySource("classpath:base.properties")
 public class GulimallCommonModule {
+
+    @Component
+    @PropertySource("classpath:dev.properties")
+    @Profile("dev")
+    public static class ProfileDev {
+
+    }
+
+    @Component
+    @PropertySource("classpath:prod.properties")
+    @Profile("prod")
+    public static class ProfileProd {
+
+    }
+
+    @Component
+    @PropertySource("classpath:test.properties")
+    @Profile("test")
+    public static class ProfileTest {
+
+    }
 }
