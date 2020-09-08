05845585ee3c2392334ab97a0228df11e87837e3
iuwjh <jerrywu555@outlook.com>
2020-09-08 10:55:25 +0800

    添加testFixtures用于存放测试用的公共类 引入embedded redis代替redis server用于测试 修正了一些配置的bug

---
+++ gulimall-common/src/testFixtures/java/com/atguigu/common/CommonTestConfiguration.java
@@ -0,0 +1,9 @@
+package com.atguigu.common;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class CommonTestConfiguration {
+}
