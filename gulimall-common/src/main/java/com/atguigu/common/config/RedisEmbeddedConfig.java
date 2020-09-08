349ca40c0b34775131d2f96f141ad1a93174b499
iuwjh <jerrywu555@outlook.com>
2020-09-08 10:55:25 +0800

    添加testFixtures用于存放测试用的公共类 引入embedded redis代替redis server用于测试 修正了一些配置的bug

---
+++ gulimall-common/src/main/java/com/atguigu/common/config/RedisEmbeddedConfig.java
@@ -0,0 +1,41 @@
+package com.atguigu.common.config;
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.net.UnknownHostException;
+
+@Configuration
+@EnableRedisRepositories
+@Profile({"test"})
+public class RedisEmbeddedConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(
+            "localhost",
+            6379);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(LettuceConnectionFactory connectionFactory)
+        throws UnknownHostException {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+}
---
+++ gulimall-common/src/testFixtures/java/com/atguigu/common/EmbeddedRedisConfiguration.java
@@ -0,0 +1,26 @@
+package com.atguigu.common;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@TestConfiguration
+public class EmbeddedRedisConfiguration {
+    private final RedisServer redisServer;
+
+    public EmbeddedRedisConfiguration() {
+        this.redisServer = new RedisServer();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+}
